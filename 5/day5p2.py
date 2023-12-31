dbaddf7395559e956af66609939b382d9f1b3fbf
gaplin <gaplinn@gmail.com>
2023-12-31 13:41:22 +0100

    5

---
+++ 5/day5p2.py
@@ -0,0 +1,24 @@
+from functools import reduce
+
+def get_row(text: str) -> int:
+    return int(reduce(lambda acc, x: acc + ('0' if x == 'F' else '1'), text, ''), 2)
+
+def get_col(text: str) -> int:
+    return int(reduce(lambda acc, x: acc + ('0' if x == 'L' else '1'), text, ''), 2)
+
+def get_seat_id(text: str) -> int:
+    row_part = text[:7]
+    col_part = text[7:]
+    row = get_row(row_part)
+    col = get_col(col_part)
+    return 8 * row + col
+
+ids = list(map(get_seat_id, open('input2.txt').read().splitlines()))
+ids.sort()
+n = len(ids)
+result = 0
+for i in range(1, n):
+    if ids[i] - ids[i - 1] == 2:
+        result = ids[i] - 1
+        break
+print(result)
